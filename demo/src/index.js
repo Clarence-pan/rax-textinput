7fa4d24fa4a957897c66d26fff4b05d41823c3b2
卓凌 <zhuoling.lcl@alibaba-inc.com>
2019-03-29 15:17:27 +0800

    chore: remove demo file

--- demo/src/index.js
+++
@@ -1,5 +0,0 @@
-import { createElement, render } from 'rax';
-import * as  DomDriver from 'driver-dom';
-import RaxTextInput from '../../src';
-
-render(<RaxTextInput />, document.body, { driver: DomDriver });
